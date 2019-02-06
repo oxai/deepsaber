858c1db0416cc0bb3fbd313bafc6763a955c67d9
Michael Trimpl <michael.trimpl@gmail.com>
2019-02-06 09:41:27 +0000

    Upload New File
    Former-commit-id: f1479ebeb8dade1b907cf4ede2334d358d9e2dd6

---
+++ example_decodeEncode.py
@@ -0,0 +1,43 @@
+from decodeJSON import parse_json
+from encodeJSON import create_dataStructure, encode_json
+from featuresBase import *
+
+file_directory_song = 'C:\\Users\micha\Dropbox\\beatsaber\\believer\Believer\song.ogg'
+file_directory = 'C:\\Users\micha\Dropbox\\beatsaber\\believer\Believer\Expert.json'
+file_directory_encode = 'C:\\Users\micha\Dropbox\\beatsaber\\believer\Believer\Expert_mod.json'
+
+
+#############################################
+# parse a json file and extrect the elements
+#############################################
+dict = parse_json(file_directory)
+
+#main variables
+events = dict['_events']
+notes = dict['_notes']
+obstacles = dict['_obstacles']
+
+#additional information
+version = dict['_version']
+shufflePeriod = dict['_shufflePeriod']
+noteJumpSpeed = dict['_noteJumpSpeed']
+beatsPerBar = dict['_beatsPerBar']
+shuffle = dict['_shuffle']
+bpm = dict['_beatsPerMinute']
+
+##############################################
+# create new features
+##############################################
+
+notes_mod = baseline_notes_simple()
+events_mod = events
+obstacles_mod = obstacles
+
+#############################################
+# encode to json
+#############################################
+
+# create the right data format (dictionary within dictionary)
+data_to_json = create_dataStructure(events_mod, notes_mod, obstacles_mod, version, shufflePeriod, noteJumpSpeed, beatsPerBar, shuffle, bpm)
+# write to file
+json_test = encode_json(data_to_json, file_directory_encode)

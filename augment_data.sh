855f08e69bc317d66a5c136040b409a62e0883d5
Guillermo Valle <guillefix@gmail.com>
2019-03-28 12:02:50 +0000

    improved bash augment data code
    Former-commit-id: 892688661b5f26aa5d51fd9a27e27ed7c2fee852

--- augment_data.sh
+++ augment_data.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-#variables for parallelizing. Divide files into 10 chunks of size 100 for parallel processing with 10 cores
-L=100
-N=10
+#mkdir AugData
 
 for i in 30 60 90; do
-    mkdir AugData/Dataup${i}; rsync -av --exclude='*.npy' DataE/* AugData/Dataup${i};
-    find AugData/Dataup${i}/ -name *.ogg | split -l $L - AugData/filesup${i}
+    #mkdir AugData/Dataup${i}; rsync -a --exclude='*.npy' DataE/* AugData/Dataup${i};
+    #find AugData/Dataup${i}/ -name *.ogg | split -l $L - AugData/filesup${i}
+    find AugData/Dataup${i}/ -name *.ogg > AugData/filesup${i}
 done
 for i in 30 60 90; do
-    mkdir AugData/Datadown${i}; rsync -av --exclude='*.npy' DataE/* AugData/Datadown${i};
-    find AugData/Datadown${i}/ -name *.ogg | split -l $L - AugData/filesdown${i}
+    #mkdir AugData/Datadown${i}; rsync -a --exclude='*.npy' DataE/* AugData/Datadown${i};
+    #find AugData/Datadown${i}/ -name *.ogg | split -l $L - AugData/filesdown${i}
+    find AugData/Datadown${i}/ -name *.ogg > AugData/filesdown${i}
 done
 
-for i in 30 60 90; do
-    for f in $(ls AugData/filesup*); do
-        ./pitch_shift_data_up $f $i &
-    done
-    sleep 1
-done
-
-for i in 30 60 90; do
-    for f in $(ls AugData/filesdown*); do
-        ./pitch_shift_data_down $f $i &
-    done
-    sleep 1
-done
-
+for i in 30 60 90; do cat AugData/filesup${i} | parallel "sox {} {}-down${i}.ogg pitch +${i}"; done
+for i in 30 60 90; do cat AugData/filesdown${i} | parallel "sox {} {}-up${i}.ogg pitch -${i}"; done

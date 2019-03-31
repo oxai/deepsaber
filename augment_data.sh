d72372aa49d7fb18590b9ab9fd81346ea1ebb094
Guillermo Valle <guillefix@gmail.com>
2019-03-31 03:30:29 +0100

    improved augment_data.sh
    Former-commit-id: 4f87ed033709454e73b25bf5b46fa4f98d2582db

--- augment_data.sh
+++ augment_data.sh
@@ -2,29 +2,6 @@
 
 #mkdir AugData
 
-#for i in 30 60 90; do
-#    #mkdir AugData/Dataup${i}; rsync -a --exclude='*.npy' DataE/* AugData/Dataup${i};
-#    #find AugData/Dataup${i}/ -name *.ogg | split -l $L - AugData/filesup${i}
-#    find AugData/Dataup${i}/ -name *.ogg > AugData/filesup${i}
-#done
-#for i in 30 60 90; do
-#    #mkdir AugData/Datadown${i}; rsync -a --exclude='*.npy' DataE/* AugData/Datadown${i};
-#    #find AugData/Datadown${i}/ -name *.ogg | split -l $L - AugData/filesdown${i}
-#    find AugData/Datadown${i}/ -name *.ogg > AugData/filesdown${i}
-#done
-
-#for i in 30 60 90; do cat AugData/filesup${i} | parallel "rm {}-down${i}.ogg"; done
-#for i in 30 60 90; do cat AugData/filesdown${i} | parallel "rm {}-up${i}.ogg"; done
-
-#for i in 30 60 90; do cat AugData/filesup${i} | parallel "echo shifting up by $(($i*10)) {}; sox {} {}-up${i}.ogg pitch +$(($i*10))"; done
-#for i in 30 60 90; do cat AugData/filesdown${i} | parallel "echo shifting down by $(($i*10)) {};sox {} {}-down${i}.ogg pitch -$(($i*10))"; done
-
-#for i in 30 60 90; do cat AugData/filesup${i} | parallel "rm {}"; done
-#for i in 30 60 90; do cat AugData/filesdown${i} | parallel "rm {}"; done
-
-#for i in 30 60 90; do find AugData/DataE -name *.ogg  | parallel "sox {} {}-up${i}.ogg pitch +$(($i*10))"; done
-#for i in 30 60 90; do find AugData/DataE -name *.ogg  | parallel "sox {} {}-down${i}.ogg pitch -$(($i*10))"; done
-
 #find AugData/DataE -name *.ogg > AugData/files
 
 for i in 30 60 90; do cat AugData/files  | parallel "echo shifting up by $(($i*10)) {}; sox {} {}-up${i}.ogg pitch +$(($i*10))"; done

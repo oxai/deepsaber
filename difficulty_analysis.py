82066beeb0b02b2f3e8ba766d24d0344e8340b33
Arcanewinds <tim.seabrook@hotmail.co.uk>
2019-04-23 22:03:05 +0100

    added functionality to extract features pertaining to: num_blocks, num_obstacles, num_bombs, num_unique_states, song_length, beats_per_minute, blocks_per_minute, blocks_per_beat
    Former-commit-id: 5a495208040e43a17c115c41b02d2484c77df6bc

--- difficulty_analysis.py
+++ difficulty_analysis.py
@@ -3,7 +3,9 @@ import os
 import re
 import html
 
-from IOFunctions import read_meta_data_file, get_list_of_downloaded_songs
+import IOFunctions
+from IOFunctions import read_meta_data_file, get_list_of_downloaded_songs, get_all_json_level_files_from_data_directory
+from identifyStateSpace import compute_explicit_states_from_bs_level
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(THIS_DIR, 'Data')
@@ -15,9 +17,9 @@ if not os.path.isdir(EXTRACT_DIR):
 
 ''' @TS: Trying to regress a model of difficulty from beatsaber level features:
 x_00) Number of blocks
-x_01) Number of unique blocks
-x_02) Number of obstacles
-x_03) Number of bombs
+x_01) Number of obstacles
+x_02) Number of bombs
+x_04) Number of unique states (bombs + blocks)
 x_04) Beats per minute
 x_05) Blocks per minute
 x_06) Blocks per beat
@@ -34,61 +36,85 @@ _Version 1_
 Linear model: y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5
 '''
 
-def extract_features_from_beatsaber_level(bsLevel):
+def extract_features_from_beatsaber_level(bs_level):
     #feature_1 = distance travelled
     #feature_2 = angles travelled
     #feature_3 = sum of product distance/angle vectors
 
-    num_blocks = extract_level_num_blocks(bsLevel)
-    num_unique_blocks = extract_level_num_unique_blocks(bsLevel)
-    num_obstacles = extract_level_num_obstacles(bsLevel)
-    num_bombs = extract_level_num_bombs(bsLevel)
+    num_blocks = extract_level_num_blocks(bs_level)
+    num_obstacles = extract_level_num_obstacles(bs_level)
+    num_bombs = extract_level_num_bombs(bs_level)
+    num_unique_states = extract_level_num_unique_states(bs_level)
 
-    beats_per_minute = extract_level_beats_per_minute(bsLevel)
-    blocks_per_minute = extract_level_blocks_per_minute(bsLevel)
-    blocks_per_beat = extract_level_blocks_per_beat(bsLevel)
-    song_length = extract_level_song_length(bsLevel)
+    beats_per_minute = extract_level_beats_per_minute(bs_level)
+    blocks_per_minute = extract_level_blocks_per_minute(bs_level)
+    blocks_per_beat = extract_level_blocks_per_beat(bs_level)
+    song_length = extract_level_song_length(bs_level)
 
-    distance_travelled = extract_level_distance_travelled(bsLevel)
-    angles_travelled = extract_level_angles_travelled(bsLevel)
-    product_distance_travelled = extract_level_product_distance_travelled(bsLevel)
+    distance_travelled = extract_level_distance_travelled(bs_level)
+    angles_travelled = extract_level_angles_travelled(bs_level)
+    product_distance_travelled = extract_level_product_distance_travelled(bs_level)
 
-    return num_blocks, num_unique_blocks, num_obstacles, num_bombs,\
+    return [num_blocks, num_obstacles, num_bombs, num_unique_states,\
            beats_per_minute, blocks_per_minute, blocks_per_beat, song_length, \
-           distance_travelled, angles_travelled, product_distance_travelled
+           distance_travelled, angles_travelled, product_distance_travelled]
 
-def extract_level_num_blocks(bsLevel):
+
+def extract_level_num_blocks(bs_level):
+    notes = bs_level["_notes"]  # Parse the JSON notes to use the notes representation
+    num_blocks = 0
+    for entry in notes.itertuples():
+        if entry[5] != 3:  # This is not a bomb
+            num_blocks += 1
+    return num_blocks
+
+
+def extract_level_num_obstacles(bs_level):
+    return bs_level['_obstacles']['values'].shape[0]
+
+
+def extract_level_num_bombs(bs_level):
+    notes = bs_level["_notes"]  # Parse the JSON notes to use the notes representation
+    num_bombs = 0
+    for entry in notes.itertuples():
+        if entry[5] == 3:  # This is a bomb
+            num_bombs += 1
+    return num_bombs
+
+
+def extract_level_num_unique_states(bs_level):
+    return len(compute_explicit_states_from_bs_level(bs_level))
+
+
+def extract_level_beats_per_minute(bs_level):
+    return bs_level['_beatsPerMinute']
+
+
+def extract_level_blocks_per_minute(bs_level):
+    return extract_level_num_blocks(bs_level) / extract_level_song_length(bs_level)
+
+
+def extract_level_blocks_per_beat(bs_level):
+    return extract_level_blocks_per_minute(bs_level) / extract_level_beats_per_minute(bs_level)
+
+
+def extract_level_song_length(bs_level):
+    notes = bs_level["_notes"]  # Parse the JSON notes to use the notes representation
+    note_times = set(notes["_time"])
+    return max(note_times)
+
+
+def extract_level_distance_travelled(bs_level):
     return 0
 
-def extract_level_num_unique_blocks(bsLevel):
+
+def extract_level_angles_travelled(bs_level):
     return 0
 
-def extract_level_num_obstacles(bsLevel):
+
+def extract_level_product_distance_travelled(bs_level):
     return 0
 
-def extract_level_num_bombs(bsLevel):
-    return 0
-
-def extract_level_beats_per_minute(bsLevel):
-    return 0
-
-def extract_level_blocks_per_minute(bsLevel):
-    return 0
-
-def extract_level_blocks_per_beat(bsLevel):
-    return 0
-
-def extract_level_song_length(bsLevel):
-    return 0
-
-def extract_level_distance_travelled(bsLevel):
-    return 0
-
-def extract_level_angles_travelled(bsLevel):
-    return 0
-
-def extract_level_product_distance_travelled(bsLevel):
-    return 0
 
 #def generate_difficulty_features():
     # Generating Data Frame
@@ -115,3 +141,7 @@ if __name__ == '__main__':
             pass
         else:
             meta_data = read_meta_data_file(meta_data_filename)
+            json_files = get_all_json_level_files_from_data_directory(os.path.join(EXTRACT_DIR, song_dir))
+            for json_file in json_files:
+                bs_level = IOFunctions.parse_json(json_file)
+                features = extract_features_from_beatsaber_level(bs_level)

86bfb1c367736158816f9b3c78cf2d7a66b4939f
Arcanewinds <tim.seabrook@hotmail.co.uk>
2019-04-23 21:26:30 +0100

    added empty functions for difficulty-related features extraction
    Former-commit-id: 72f8b630940dc8ee8b94babd576f381d86ca2ce2 [formerly 392d3aba899122f8651f622dd76c99cd76e4f163]
    Former-commit-id: 86fccbb30182755fd141da45f8eca576530725d6

--- difficulty_analysis.py
+++ difficulty_analysis.py
@@ -14,12 +14,17 @@ if not os.path.isdir(EXTRACT_DIR):
     os.mkdir(EXTRACT_DIR)
 
 ''' @TS: Trying to regress a model of difficulty from beatsaber level features:
-x_1) Number of blocks
-x_2) Number of unique blocks
-x_3) Number of obstacles
-x_4) Number of bombs
-x_5) Beats per minute
-x_6) Song length
+x_00) Number of blocks
+x_01) Number of unique blocks
+x_02) Number of obstacles
+x_03) Number of bombs
+x_04) Beats per minute
+x_05) Blocks per minute
+x_06) Blocks per beat
+x_07) Song length
+x_08) Distance Travelled
+x_09) Angles Travelled
+x_10) Product Distance Travelled
 
 y_0) Difficulty rating
 
@@ -28,6 +33,62 @@ z_0) Scoresaber difficulty rating
 _Version 1_
 Linear model: y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5
 '''
+
+def extract_features_from_beatsaber_level(bsLevel):
+    #feature_1 = distance travelled
+    #feature_2 = angles travelled
+    #feature_3 = sum of product distance/angle vectors
+
+    num_blocks = extract_level_num_blocks(bsLevel)
+    num_unique_blocks = extract_level_num_unique_blocks(bsLevel)
+    num_obstacles = extract_level_num_obstacles(bsLevel)
+    num_bombs = extract_level_num_bombs(bsLevel)
+
+    beats_per_minute = extract_level_beats_per_minute(bsLevel)
+    blocks_per_minute = extract_level_blocks_per_minute(bsLevel)
+    blocks_per_beat = extract_level_blocks_per_beat(bsLevel)
+    song_length = extract_level_song_length(bsLevel)
+
+    distance_travelled = extract_level_distance_travelled(bsLevel)
+    angles_travelled = extract_level_angles_travelled(bsLevel)
+    product_distance_travelled = extract_level_product_distance_travelled(bsLevel)
+
+    return num_blocks, num_unique_blocks, num_obstacles, num_bombs,\
+           beats_per_minute, blocks_per_minute, blocks_per_beat, song_length, \
+           distance_travelled, angles_travelled, product_distance_travelled
+
+def extract_level_num_blocks(bsLevel):
+    return 0
+
+def extract_level_num_unique_blocks(bsLevel):
+    return 0
+
+def extract_level_num_obstacles(bsLevel):
+    return 0
+
+def extract_level_num_bombs(bsLevel):
+    return 0
+
+def extract_level_beats_per_minute(bsLevel):
+    return 0
+
+def extract_level_blocks_per_minute(bsLevel):
+    return 0
+
+def extract_level_blocks_per_beat(bsLevel):
+    return 0
+
+def extract_level_song_length(bsLevel):
+    return 0
+
+def extract_level_distance_travelled(bsLevel):
+    return 0
+
+def extract_level_angles_travelled(bsLevel):
+    return 0
+
+def extract_level_product_distance_travelled(bsLevel):
+    return 0
 
 #def generate_difficulty_features():
     # Generating Data Frame

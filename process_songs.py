675688ddf8de7d5b2712760e7e03896ebb5e0a9b
Guillermo Valle <guillefix@gmail.com>
2019-05-12 19:55:50 +0100

    process_songs with syncced features
    Former-commit-id: efc16416c60da563562d7d6f0a48d17c07acd3c6

--- process_songs.py
+++ process_songs.py
@@ -12,7 +12,10 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 print(rank)
 
-data_path = Path("AugData")
+import sys
+# sys.argv[1]="AugDataTest/"
+# sys.argv[2]="Expert"
+data_path = Path(sys.argv[1])
 #data_path = Path("DataE/")
 
 candidate_audio_files = sorted(data_path.glob('**/*.ogg'), key=lambda path: path.parent.__str__())
@@ -28,30 +31,30 @@ if rank < num_tasks%size:
 
 feature_name = "chroma"
 feature_size = 24
+use_sync=True
 
-difficulty = "Hard"
+difficulties = sys.argv[2]
 sampling_rate = 16000
 beat_subdivision = 16
-n_mfcc = 20
+# n_mfcc = 20
 
-time_shifts = 16
+# path = candidate_audio_files[0]
 
-# path = candidate_audio_files[4]
+#%%
 
 for i in tasks:
     path = candidate_audio_files[i]
     #print(path)
     song_file_path = path.__str__()
     features_file = song_file_path+"_"+feature_name+"_"+str(feature_size)+".npy"
-    try:
-        level = list(path.parent.glob('./'+difficulty+'.json'))[0]
-        level = level.__str__()
-    except (TypeError, IndexError):
-            continue
+    for diff in difficulties.split(","):
+        if Path(path.parent.__str__()+"/"+diff+".json").is_file():
+            level = list(path.parent.glob('./'+diff+'.json'))[0]
+            level = level.__str__()
+            level_file_found = True
+    if not level_file_found:
+        continue
     if not os.path.isfile(features_file):
-        #mfcc = pickle.load(open(mfcc_file,"rb"))
-        #print("found mfcc file already")
-
         print("creating feature file",i)
         level = json.load(open(level, 'r'))
 
@@ -60,17 +63,29 @@ for i in tasks:
 
         bpm = level['_beatsPerMinute']
         sr = sampling_rate
-        beat_duration = int(60*sr/bpm) #beat duration in samples
+        # beat_duration = int(60*sr/bpm) #beat duration in samples
+        #
+        # #hop = beat_duration//beat_subdivision #one vec of mfcc features per 16th of a beat (hop is in num of samples)
+        # hop = int(beat_duration * 1/beat_subdivision)
+        # hop -= hop % 32
+        # num_samples_per_feature = hop
+        # mel_window = hop
 
-        #hop = beat_duration//beat_subdivision #one vec of mfcc features per 16th of a beat (hop is in num of samples)
-        hop = int(beat_duration * 1/beat_subdivision)
-        hop -= hop % 32
-        num_samples_per_feature = hop
-        mel_window = hop
+        #get feature
+        #features = feature_extraction_hybrid_raw(y_wav,sr,bpm)
+        if use_sync:
+            features = feature_extraction_hybrid_raw(y_wav,sr,bpm)
+        else:
+            features = feature_extraction_hybrid(y_wav,sr,bpm)
+        np.save(features_file,features)
 
-        notes = level['_notes']
-        # from math import floor
+        ## get mfcc feature
+        # mfcc = librosa.feature.mfcc(y_wav, sr=sr, hop_length=mel_hop, n_fft=mel_window, n_mfcc=n_mfcc)
+        # y = mfcc
+
         # uncomment to look for notes beyond the end of time
+        # notes = level['_notes']
+        # from math import floor
         # for note in notes:
         #     sample_index = floor((note['_time']*60/bpm)*sr/num_samples_per_feature)
         #     # check if note falls within the length of the song (why are there so many that don't??) #TODO: research why this happens
@@ -78,15 +93,6 @@ for i in tasks:
         #         print("note beyond the end of time")
         #         print((note['_time']*60/bpm)-(y_wav.shape[0]/sr))
         #         continue
-
-        #get feature
-        #features = feature_extraction_hybrid_raw(y_wav,sr,bpm)
-        features = feature_extraction_hybrid_raw(y_wav,sr,bpm)
-        np.save(features_file,features)
-
-        ## get mfcc feature
-        # mfcc = librosa.feature.mfcc(y_wav, sr=sr, hop_length=mel_hop, n_fft=mel_window, n_mfcc=n_mfcc)
-        # y = mfcc
 
 # import matplotlib.pyplot as plt
 # %matplotlib
@@ -96,8 +102,9 @@ for i in tasks:
 #
 # sampling_rate = 22050
 # y_wav, sr = librosa.load(song_file_path, sr=sampling_rate)
-# features = np.load(features_file)
 # features = feature_extraction_hybrid_raw(y_wav,sr,bpm)
+# features = np.load(features_file)
+# import librosa.display
 # librosa.display.specshow(features[:12,:],x_axis='time')
 # librosa.display.specshow(features[12:,:],x_axis='time')
 # sample_index

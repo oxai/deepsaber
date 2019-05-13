088b34bb82147eb84ef91ae55415ac3fd8cf1081
Guillermo Valle <guillefix@gmail.com>
2019-05-13 02:38:23 +0100

    process songs sync features
    Former-commit-id: 7e22d30a01f92fd9933cb775640d31f0e05f0682

--- upload_features_to_dropbox.sh
+++ upload_features_to_dropbox.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+day=yesterday
+
 feature="chroma_24"
-folder_name="chroma_24_"$(date +%F)
+folder_name="chroma_24_"$(date --date="${day}" +%F)
 
 #find AugData -type d | sed -n 's|^AugData/||p' | parallel 'mkdir -p AugData_'${feature}'_features/{}'
 find AugData/ -type d -printf '%P\0' | parallel -0 'mkdir -p AugData_'${folder_name}'_features/{}'
 
 #find AugData/ -name "*_"${feature}".npy" | sed -n 's|^AugData/||p' | parallel 'cp AugData/{} ./AugData_'${feature}'_features/{}'
-find AugData/ -type f -name "*_"${feature}".npy" -newermt $(date +%F) -printf '%P\0' | parallel -0 'cp AugData/{} ./AugData_'${folder_name}'_features/{}'
+find AugData/ -type f -name "*_"${feature}".npy" -newermt $(date --date="${day}" +%F) -printf '%P\0' | parallel -0 'cp AugData/{} ./AugData_'${folder_name}'_features/{}'
 
 tar --use-compress-program="pigz --best --recursive | pv" -cf AugData_${folder_name}_features.tar.gz AugData_${folder_name}_features/
 

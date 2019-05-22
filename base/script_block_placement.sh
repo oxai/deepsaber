b2d0bbdfef53b4df3598a887cd4b5e1aadf0a980
Guillermo Valle <guillefix@gmail.com>
2019-05-22 23:33:38 +0100

    script_block_placement.sh
    Former-commit-id: 9e0c640213aad4f0f0e9366290c62ae4e461a6b6 [formerly 27148941d1e6f5486b9a277da6d45480e85130d2]
    Former-commit-id: 105f59e1b99bd5f3585444dc6175a9508fe306ad

--- base/script_block_placement.sh
+++ base/script_block_placement.sh
@@ -7,15 +7,17 @@ dataset=general_beat_saber
 model=wavenet
 layers=7
 blocks=3
-exp=test
+exp=block_placement
 num_windows=10
 
-$py train.py --data_dir=../AugDataTest --dataset_name=$dataset --model=$model --batch_size=5 --output_length=128 --num_windows=$num_windows --nepoch=500 --nepoch_decay=500 --layers=$layers --blocks=$blocks \
-    --print_freq=1 --experiment_name=$exp --save_by_iter --save_latest_freq=10 \
+$py train.py --data_dir=../AugData --dataset_name=$dataset --model=$model --batch_size=5 --output_length=128 --num_windows=$num_windows --nepoch=500 --nepoch_decay=500 --layers=$layers --blocks=$blocks \
+    --print_freq=1 --experiment_name=$exp --save_by_iter --save_latest_freq=1000 \
+    --val_epoch_freq=0 \
     --time_shifts=16 \
     --feature_name=mel \
     --feature_size=100 \
-    --input_channels=$((100*16)) \
+    --concat_outputs \
+    --input_channels=$((100*16+1+4)) \
     --num_classes=$((1+4)) \
     --extra_output \
     --using_sync_features \
@@ -24,12 +26,11 @@ $py train.py --data_dir=../AugDataTest --dataset_name=$dataset --model=$model --
     --level_diff=Expert \
     --reduced_state \
     --binarized \
-    --gpu_ids=0
+    --gpu_ids=0 \
+    --dilation_channels=512 \
+    --residual_channels=256 \
+    --skip_channels=256 \
+    --end_channels=512 \
     #--load \
-    #--load_iter=1070 \
-    # --dilation_channels=512 \
-    # --residual_channels=256 \
-    # --skip_channels=256 \
-    # --end_channels=512 \
-    #--concat_outputs \
+    #--load_iter=51200 \
     # --gpu_ids=0,1,2,3,4,5,6,7 \

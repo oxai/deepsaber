249fc5179080b7c810f5c2b5e1734c8c4f4bbae9
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-07 20:23:49 +0000

    new_scripts
    Former-commit-id: cda7d0f43151196a89d99ada5f94743164f2365f [formerly 5a7fa8a14e71c4a10ca0f7fa2bce362b6dd36dee]
    Former-commit-id: 357531881a843c087f72121046e62dc9ee4de720

--- base/script_block_placement.sh
+++ base/script_block_placement.sh
@@ -8,12 +8,13 @@ dataset=general_beat_saber
 model=wavenet
 layers=5
 blocks=3
-exp=block_placement_new
+exp=block_placement_new_nohumreg
 num_windows=10
 
-$py train.py --data_dir=../AugData --dataset_name=$dataset --model=$model --batch_size=2 --output_length=1 --num_windows=$num_windows --nepoch=500 --nepoch_decay=500 --layers=$layers --blocks=$blocks \
-    --print_freq=10 --experiment_name=$exp --save_by_iter --save_latest_freq=5000 \
+$py train.py --data_dir=../AugData --dataset_name=$dataset --model=$model --batch_size=10 --output_length=1 --num_windows=$num_windows --nepoch=500 --nepoch_decay=500 --layers=$layers --blocks=$blocks \
+    --print_freq=10 --experiment_name=$exp --save_by_iter --save_latest_freq=10000 \
     --val_epoch_freq=0 \
+    --humaneness_reg_coeff=0.0 \
     --feature_name=mel \
     --feature_size=100 \
     --concat_outputs \
@@ -25,12 +26,12 @@ $py train.py --data_dir=../AugData --dataset_name=$dataset --model=$model --batc
     --reduced_state \
     --binarized \
     --gpu_ids=0 \
+    --continue_train \
+    --load_iter=930000 \
     #--dilation_channels=512 \
     #--residual_channels=256 \
     #--skip_channels=256 \
     #--end_channels=512 \
-    #--continue_train \
-    #--load_iter=68000 \
     #--load \
     # --gpu_ids=0,1,2,3,4,5,6,7 \
 

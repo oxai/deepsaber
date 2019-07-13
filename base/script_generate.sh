9d38d0d3393a1a5a6c58648bfe38d6f3718f6309
Guillermo Valle <guillefix@gmail.com>
2019-07-13 22:51:11 +0100

    refactored generate a bit, and made BeastSaber scrapper
    Former-commit-id: 7cfbbad05cd74df99af1c7a130c950395d9dd371 [formerly dfe15cfbf94f0ebff7b902e257f22855b7da26ed]
    Former-commit-id: 7ab8c1d95f6de5ad584cdc253346b40bff2c388e

--- base/script_generate.sh
+++ base/script_generate.sh
@@ -7,7 +7,8 @@ cpt1=1220000
 #cpt1=755000
 exp2=block_selection_new
 cpt2=1600000
-ddc_file=/home/guillefix/ddc_infer/57257860-f345-4e5c-ba69-36f57b561118/57257860-f345-4e5c-ba69-36f57b561118.sm
+#ddc_file=/home/guillefix/ddc_infer/57257860-f345-4e5c-ba69-36f57b561118/57257860-f345-4e5c-ba69-36f57b561118.sm
+ddc_file=$2
 
 python3 generate.py --song_path $song_path --experiment_name $exp1 --checkpoint $cpt1 --experiment_name2 $exp2 --checkpoint2 $cpt2 --two_stage \
     --peak_threshold 0.007 \

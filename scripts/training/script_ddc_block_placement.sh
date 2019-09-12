0c1876c454b91640ca60ea0bc8b50e9a9702f12d
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-12 00:36:16 +0000

    gcloud changes
    Former-commit-id: 8c4e3b760fcfe58ba22482b6d0f82e7d506afe61 [formerly fb270718d45c1399c6e75066e71e309d9f29c800]
    Former-commit-id: aeb296410ec36b029aabb3ec745360cad421a894

--- scripts/training/script_ddc_block_placement.sh
+++ scripts/training/script_ddc_block_placement.sh
@@ -6,26 +6,24 @@ py=/usr/bin/python3
 #py=/media/usr/bin/python3
 dataset=general_beat_saber
 model=ddc
-layers=7
-blocks=3
 exp=block_placement_ddc
-num_windows=10
+num_windows=1
 
-$py train.py --data_dir=../../data/extracted_data --dataset_name=$dataset --model=$model --batch_size=1 \
+$py train.py --data_dir=../../data/extracted_data --dataset_name=$dataset --model=$model --batch_size=10 \
     --num_windows=$num_windows --nepoch=500 --nepoch_decay=500 \
-    --print_freq=10 --experiment_name=$exp --save_by_iter --save_latest_freq=10000 --time_offset=0\
-    --time_shifts=15\
+    --print_freq=10 --experiment_name=$exp --save_by_iter --save_latest_freq=10000 --time_offset=0 \
+    --time_shifts=15 \
     --output_length=100 \
     --val_epoch_freq=0 \
     --feature_name=multi_mel \
     --feature_size=80 \
     --num_classes=$((1+4)) \
-    --workers=32 \
-    --level_diff=Expert \
+    --workers=16 \
+    --level_diff=Expert,ExpertPlus \
     --reduced_state \
     --binarized \
      --gpu_ids=0,1,2,3,4,5,6,7 \
-    # --continue_train \
-    # --load_iter=930000 \
+    --continue_train \
+    --load_iter=50000 \
     #--gpu_ids=0 \
     # --input_channels=$((80+4)) \
